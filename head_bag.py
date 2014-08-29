d3db11970ef03471dea89da1560c8e3c36cdcd92
Tully Foote <tfoote@osrfoundation.org>
2014-08-29 15:11:12 -0700

    remove old retimestamping logic

--- head_bag.py
+++ head_bag.py
@@ -7,9 +7,4 @@ with rosbag.Bag('head.bag', 'w') as outbag:
         if num_msgs < 1:
             break
         num_msgs -= 1
-        # This also replaces tf timestamps under the assumption
-        # that all transforms in the message share the same timestamp
-        if topic == "/tf" and msg.transforms:
-            outbag.write(topic, msg, msg.transforms[0].header.stamp)
-        else:
-            outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+        outbag.write(topic, msg, t)
